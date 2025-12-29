c1272cbae7fd05f0627694ef1ae3ebaf9cf1e5a9
lalithk90 <asakahatapitiya@gmail.com>
2025-12-30 01:03:26 +0700

    app was working

---
+++ braintumoridentificationapp/src/config/appInfo.ts
@@ -0,0 +1,29 @@
+export const appInfo = {
+  researchTitle: 'AI HEALTH ASSISTANT FOR BRAIN TUMOR SEGMENTATION AND INTERPRETATION',
+  institute: 'POSTGRADUATE INSTITUTE OF SCIENCE',
+  researcherName: 'K. R. A. L. Kahatapitiya',
+  registrationNumber: 'PGIS/SC/M/CSC/2020/21',
+  boardOfStudy: 'Board of Study in Statistics and Computer Science',
+  thesisPresentedByLabel: 'A THESIS PRESENTED BY',
+  degreeName: 'MASTER OF SCIENCE IN COMPUTER SCIENCE',
+  university: 'UNIVERSITY OF PERADENIYA',
+  country: 'SRI LANKA',
+  year: '2025',
+  supervisors: [
+    'Dr. Ruwan D. Nawarathna',
+    'Prof. Roshan Darshana Yapa',
+    'Prof. Amalka Pinidiyaarachchi'
+  ],
+  disclaimer:
+    'This application is developed for research purposes only. It is not intended for medical diagnosis, treatment decisions, or clinical use.',
+  note:
+    'This study is conducted under PGIS with datasets used in accordance with their licenses and ethical considerations.',
+  dataset: {
+    name: 'Brain Tumor MRI Dataset',
+    source: 'Kaggle',
+    url: 'https://www.kaggle.com/datasets/masoudnickparvar/brain-tumor-mri-dataset',
+    licenseNote: 'Open dataset used for academic research purposes with attribution to the original authors and sources (Figshare, SARTAJ, Br35H).'
+  }
+};
+
+export default appInfo;
